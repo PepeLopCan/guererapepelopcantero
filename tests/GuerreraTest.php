523b08aadae7d154222ee2f8b2b68f65dd7cb06a
PepeLopCan <pepelopcan21@gmail.com>
2022-02-02 12:50:05 +0100

    1

---
+++ tests/GuerreraTest.php
@@ -0,0 +1,45 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class GuerreraTest extends TestCase{
+
+    private $op;
+
+    public function setUp():void{
+        $this->op = new Guerrera('Pepe',12,'viva');
+    }
+
+    public function testVida(){
+        $this->op->herida();
+        $this->assertEquals("viva",$this->op->getEstado());
+        $this->assertEquals(2,$this->op->getVida());
+    }
+
+    public function testPocavida(){
+        $this->op->herida();
+        $this->op->herida();
+        $this->assertEquals("muerta",$this->op->getEstado());
+        $this->assertEquals(-2,$this->op->getVida());
+    }
+
+
+    public function testPoti1(){
+       for ($i=0; $i < 3; $i++) { 
+        $this->op->herida();
+       }
+        $this->op->pocion();
+        $this->assertEquals("muerta",$this->op->getEstado());
+        $this->assertEquals(-5,$this->op->getVida());
+    }
+
+    public function testPoti2(){
+       
+        for ($i=0; $i < 2; $i++) { 
+            $this->op->herida();
+           }
+        $this->op->pocion();
+        $this->assertEquals("viva",$this->op->getEstado());
+        $this->assertEquals(5,$this->op->getVida());
+    }
+
+}
